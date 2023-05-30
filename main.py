99e90ea2674a062bd35d26606dedf9675e845045
Paul Haedrich <paul@berrysauce.me>
2023-05-30 22:27:03 +0200

    Added colors 🌈

--- main.py
+++ main.py
@@ -16,9 +16,14 @@ app.mount("/assets", StaticFiles(directory="templates/assets"), name="assets")
 # - FUNCTIONS - #
 
 def binarize(string: str):
-    hash = hashlib.sha256(bytes(string, "utf-8")).hexdigest() # Create a SHA256 hash of the input string
-    decimal_value = int(hash, 16)  # Convert hex string to decimal value
-    binary_string = bin(decimal_value)[2:]  # Convert decimal value to binary string, excluding the '0b' prefix
+    # Create a SHA256 hash of the input string
+    hash = hashlib.sha256(bytes(string, "utf-8")).hexdigest()
+    
+    # Convert hex string to decimal value
+    decimal_value = int(hash, 16)
+    
+    # Convert decimal value to binary string, excluding the '0b' prefix
+    binary_string = bin(decimal_value)[2:]
     
     # increase string length to 256 if not long enoughs
     while len(binary_string) < 256:
@@ -43,37 +48,78 @@ def get_docs():
 def get_identicon(data: str, color: Union[str, None] = None, size: Union[int, int] = 250):
     binarized = binarize(data)
 
-    color_data = binarized[:58] # Trim to 66 bits
-    field_data = binarized[58:] # Trim to 190 bits
+    color_data = binarized[:58] # Trim to 58 bits
+    field_data = binarized[58:] # Trim to 198 bits
     
     if color == None:
-        color = "#5bc0eb"
+        segment_length = 6  # Length of each segment
+        
+        color_map = {
+            0: "#5bc0eb", # blue - my fav :)
+            1: "#fde74c", # yellow
+            2: "#9bc53d", # green
+            3: "#e55934", # red
+            4: "#fa7921"  # orange
+        }
+        
+        # get the first 6 bits of the color data
+        color_data_segment = color_data[:6]
+        
+        # Convert the segment from binary to decimal
+        decimal_value = int(color_data_segment, 2)
+        
+        # Use hashlib to generate a hash value based on the segment
+        hash_value = hashlib.md5(str(decimal_value).encode()).hexdigest()
+        
+        # Convert the hash value to an integer
+        hash_integer = int(hash_value, 16)
+        
+        # Map the integer value to an index within the range of available colors
+        color_index = hash_integer % len(color_map)
+        
+        # Get the color based on the index
+        color = color_map[color_index]
+        
     else:
         color = "#" + color    
     
     fields = []
+    
     for i in range(66):
         fields.append(field_data[:3]) # get first 3 bits
         field_data = field_data[3:] # then remove them
 
     field_fill = []
+    
     for field in fields:
-        bit_list = list(field) # convert bits to list (010 -> [0, 1, 0])
-        bit_sum = int(bit_list[0]) + int(bit_list[1]) + int(bit_list[2]) # sum all bits
+        # convert bits to list (010 -> [0, 1, 0])
+        bit_list = list(field)
+        
+        # sum all bits
+        bit_sum = int(bit_list[0]) + int(bit_list[1]) + int(bit_list[2])
         
         if bit_sum <= 1:
             field_fill.append(False)
         elif bit_sum >= 2:
             field_fill.append(True)
 
-    usable_grid_size = [5, 5, 3] # x, y, x-limit (see comments above) (max: 11,11,6)
-    dwg = svgwrite.Drawing("identicon.svg", profile="tiny") # credits to ChatGPT lol, didn't know this existed
-    cell_size = size / usable_grid_size[0]  # Size of each identicon cell
-    for i in range(usable_grid_size[1]): # iterate through y
+    # x, y, x-limit (see comments above) (max: 11,11,6)
+    usable_grid_size = [5, 5, 3]
+    
+    # credits to ChatGPT lol, didn't know this existed
+    dwg = svgwrite.Drawing("identicon.svg", profile="tiny")
+    
+    # Size of each identicon cell
+    cell_size = size / usable_grid_size[0]
+    
+    # iterate through y
+    for i in range(usable_grid_size[1]):
         row_list = []
         
-        for j in range(usable_grid_size[2]): # iterate through x
-            if field_fill[i*usable_grid_size[2]+j] == True: # i (row) * x (size, e.g. 11) + j (column index) -> list index
+        # iterate through x
+        for j in range(usable_grid_size[2]):
+            # i (row) * x (size, e.g. 11) + j (column index) -> list index
+            if field_fill[i*usable_grid_size[2]+j] == True:
                 # Calculate cell position
                 x = j * cell_size
                 y = i * cell_size
@@ -81,11 +127,17 @@ def get_identicon(data: str, color: Union[str, None] = None, size: Union[int, in
                 # Draw cell rectangle with the assigned color
                 dwg.add(dwg.rect((x, y), (cell_size, cell_size), fill=color))
             else:
-                pass # pass instead of continue because continue would skip the row_list appendding
-            row_list.append(field_fill[i*usable_grid_size[2]+j]) # make a speperate list for reversing
+                pass # pass instead of continue because continue would skip the row_list appending
+            
+            # make a speperate list for reversing
+            row_list.append(field_fill[i*usable_grid_size[2]+j])
         
-        row_list_reversed = list(reversed(row_list))[1:] # reverse the list & remove the first element (the middle one / x-limit)
-        row_list_index = 0 # make a seperate index for the reversed list since k is not an index like j
+        # reverse the list & remove the first element (the middle one / x-limit)
+        row_list_reversed = list(reversed(row_list))[1:]
+        
+        # make a seperate index for the reversed list since k is not an index like j
+        row_list_index = 0
+        
         for k in row_list_reversed:
             if k == True:
                 # Calculate cell position
